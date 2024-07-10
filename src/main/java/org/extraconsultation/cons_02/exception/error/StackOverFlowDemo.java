30ad53e9574c0f852cb39e1f360789abfdde15e0
StanislavPB <perminovsb@gmail.com>
2024-07-10 20:43:56 +0100

    exception

---
+++ src/main/java/org/extraconsultation/cons_02/exception/error/StackOverFlowDemo.java
@@ -0,0 +1,17 @@
+package org.extraconsultation.cons_02.exception.error;
+
+public class StackOverFlowDemo {
+    public static void main(String[] args) {
+        StackOverFlowDemo stack = new StackOverFlowDemo();
+
+        int count = 0;
+        stack.stackOn(count);
+
+    }
+
+    public void stackOn(int count){
+        count++;
+        System.out.print(count + "; ");
+        stackOn(count);
+    }
+}
